1131e5de5b1acd5b2d75dc5081a27edb2ed48982
akskoon <akisaki.11to5@icloud.com>
2021-04-18 15:42:37 +0900

    カウンター

---
+++ 1_workspace/1/カウンター/main.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+const display = document.getElementById("display")
+const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const dblButton = document.getElementById("dbl-button")
+
+let count = 0
+
+plusButton.onclick = function() {
+  count += 1
+  display.textContent = count
+}
+
+minusButton.onclick = function() {
+  count -= 1
+  display.textContent = count
+}
+
+dblButton.onclick = function() {
+  count = count * 2
+  display.textContent = count
+}
